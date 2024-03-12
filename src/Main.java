2a880211c60135b4e84898f62fd24b0031fd82a1
Petr K <flyingship1@gmail.com>
2024-03-12 23:45:47 +0300

    Added file path parser

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        int fileCount = 0;
+
+        while (true) {
+            System.out.println("Введите путь к файлу: ");
+
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if(!fileExists || isDirectory) {
+                System.out.println("Файл " + path + " не существует или путь является путем к папке");
+                continue;
+            }
+            if(fileExists) {
+                System.out.println("Путь " + path + " указан верно");
+                fileCount++;
+                System.out.println("Это файл номер " + fileCount);
+            }
+        }
     }
 }
